6bd6a52d52fd7c1b9d2da801546e3cd66b4ae34f
Nikita Ignatovich <nikita@ignatovich.me>
2021-08-23 11:41:16 +0300

    Added pages titles

--- src/router/index.js
+++ src/router/index.js
@@ -5,16 +5,25 @@ const routes = [
     path: '/',
     name: 'Home',
     component: () => import(/* webpackChunkName: "pages" */ '../views/Pages/Home.vue'),
+    meta: {
+      title: 'Панель управления',
+    },
   },
   {
     path: '/users',
     name: 'Users',
     component: () => import(/* webpackChunkName: "users" */ '../views/Users/Main.vue'),
+    meta: {
+      title: 'Пользователи',
+    },
   },
   {
     path: '/places',
     name: 'Places',
     component: () => import(/* webpackChunkName: "places" */ '../views/Places/Main.vue'),
+    meta: {
+      title: 'Места',
+    },
   },
 ];
 
@@ -22,6 +31,13 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   linkActiveClass: 'active',
   routes,
+});
+
+router.beforeEach((to) => {
+  const pageTitle = to.meta.title || 'Страница';
+  document.title = `${pageTitle} | BP Admin`;
+
+  return true;
 });
 
 export default router;
