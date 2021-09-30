af054deaa0d057ccde03fd8273830c7d0a88948a
Nikita Ignatovich <nikita@ignatovich.me>
2021-09-30 11:19:05 +0300

    Automatic close menu in mobile view then taped on link

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+  <nav  id="sidebarMenu"
+        class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+        :key="$route.path">
     <div class="position-sticky pt-3">
       <ul class="nav flex-column">
         <li class="nav-item">
